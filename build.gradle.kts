42cf42fbe82908da380021af1518b37f5ebc9f89
A-man404 <amansaini04a@gmail.com>
2026-08-03 15:22:04 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply  false
+    alias(libs.plugins.vanniktech.mavenPublish) apply false
+    id("nl.littlerobots.version-catalog-update") version "1.1.1"
+}
